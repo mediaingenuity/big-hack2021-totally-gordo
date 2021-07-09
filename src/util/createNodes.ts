ab28c4496ff791e2d0e0bf9a15563e17963a7af4
ac1989 <axcarter89@gmail.com>
2021-07-09 10:42:50 +0100

    discrad json

--- src/util/createNodes.ts
+++ src/util/createNodes.ts
@@ -1,29 +1,34 @@
-import { max, percentage } from "../util/basicMath"
+import { max, percentage } from "../util/basicMath";
 
 const getMaxValue = (d) =>
-  d.reduce((prev, current) => (prev.size > current.size ? prev : current))
+  d.reduce((prev, current) => (prev.size > current.size ? prev : current));
+
+const SUMWithoutJSON = (sum, json) => {
+  if (!json) return sum;
+  return sum - json;
+};
 
 export const createNodes = (d) => {
-  const nodes = []
+  const nodes = [];
   d.repos.forEach((node, index) => {
-    const children = []
+    const children = [];
     Object.keys(node.loc).forEach((item) => {
-      if (item !== "SUM") {
+      if (item !== "SUM" && item !== "JSON") {
         children.push({
           name: item,
           size: node.loc[item],
           percentage: percentage(node.loc[item], node.loc),
-        })
+        });
       }
-    })
+    });
     nodes.push({
       id: index,
       name: node.name,
       children: children,
       maxValue: getMaxValue(children),
-      size: +node.loc.SUM,
-    })
-  })
+      size: SUMWithoutJSON(+node.loc.SUM, +node.loc.JSON),
+    });
+  });
 
-  return nodes
-}
+  return nodes;
+};
