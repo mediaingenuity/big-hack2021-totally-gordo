f04eaa72caae8aa7e8d73f044ff12c632cea529c
Aaron Krohn <akrohn@totallymoney.com>
2021-07-09 09:58:17 +0100

    add date query param check

--- src/pages/languages.jsx
+++ src/pages/languages.jsx
@@ -18,13 +18,25 @@ const Languages = () => {
     const [data, setData] = useState(null)
     const [notfound, setNotFound] = useState(false)
     const [queryRepo, _] = useQueryParam("repo", StringParam)
+    const [queryDate] = useQueryParam("date", StringParam)
 
     useEffect(() => {
         if (JSONData) {
-            const filterRepo = JSONData[0].repos.filter((repo) => repo.name.toLowerCase() === queryRepo.toLowerCase())
+            const findDate = JSONData?.find((datum) => {
+                return datum.date === queryDate
+            })
+
+
+            if (!findDate && findDate?.length !== 1) {
+                setNotFound(true)
+
+                return
+            }
+
+            const filterRepo = findDate.repos.filter((repo) => repo.name.toLowerCase() === queryRepo.toLowerCase())
 
             if (filterRepo.length === 1) {
-                setData(JSONData[0].repos.filter((repo) => repo.name.toLowerCase() === queryRepo.toLowerCase()))
+                setData(findDate.repos.filter((repo) => repo.name.toLowerCase() === queryRepo.toLowerCase()))
             } else {
                 setNotFound(true)
             }
@@ -42,7 +54,7 @@ const Languages = () => {
                             <DonutComponent x={45} y={20} data={data[0]} />
                         </svg>
                     </>
-                ) : notfound ? (<p>Repository not found, check query param</p>) : (<p>Loading</p>)
+                ) : notfound ? (<p>Repository not found (Needs repo and date query param)</p>) : (<p>Loading</p>)
                 }
             </Page>
         </Layout>
